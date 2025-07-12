e9477e454b05d51ac48551d194e05c6bbdca4829
angelreyes117 <angelreyes.117@gmail.com>
2025-07-12 04:27:36 +0000

    NoSQL

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,30 +1,19 @@
 #!/usr/bin/env python3
-"""  provides some stats about
-Nginx logs stored in MongoDB """
-
+"""Print info f nginx logs """
 from pymongo import MongoClient
 
-
-def log_stats(log_dict: dict) -> int:
-    """ provides some stats about Nginx logs stored in MongoDB
-        - Database: logs
-        - Collection: nginx
-        - Methods ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    """
+if __name__ == "__main__":
+    """ check for all elements in a collection """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    data = client.logs.nginx
-    logs = data.count_documents({log_dict})
-    message = f"\
-              { log_stats({logs}) } logs\n\
-              Methods:\n\
-              \tmethod GET: { log_stats({'method': 'GET'}) }\n\
-              \tmethod POST: { log_stats({'method': 'POST'}) }\n\
-              \tmethod PUT: {log_stats({'method': 'PUT'})}\n\
-              \tmethod PATCH: {log_stats({'method': 'PATCH'})}\n\
-              \tmethod DELETE: {log_stats({'method': 'DELETE'})}\n\
-              {status} status check\
-            "
-    print(message)
+    collection = client.logs.nginx
 
-    if __name__ == "__main__":
-        log_stats()
+    print(f"{collection.estimated_document_count()} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {method_count}")
+
+    check_get = collection.count_documents(
+        {'method': 'GET', 'path': "/status"})
+    print(f"{check_get} status check")
