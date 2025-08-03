205e4fcb70b2b1d5b99b0ed2835624e28133a596
angelreyes117 <angelreyes.117@gmail.com>
2025-08-03 22:14:51 +0000

    ES6 classes

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
     'max-classes-per-file': 'off',
     'no-underscore-dangle': 'off',
