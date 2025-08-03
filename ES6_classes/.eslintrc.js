84f0cb1ab92da3e539002fb9ac95c356ab88326d
angelreyes117 <angelreyes.117@gmail.com>
2025-08-03 22:06:26 +0000

    ES6 classes

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -2,11 +2,9 @@ module.exports = {
   env: {
     browser: false,
     es6: true,
-    jest: true,
   },
   extends: [
     'airbnb-base',
-    'plugin:jest/all',
   ],
   globals: {
     Atomics: 'readonly',
@@ -16,7 +14,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['jest'],
+  plugins: [],
   rules: {
     'max-classes-per-file': 'off',
     'no-underscore-dangle': 'off',
@@ -28,7 +26,7 @@ module.exports = {
       'WithStatement',
     ],
   },
-  overrides:[
+  overrides: [
     {
       files: ['*.js'],
       excludedFiles: 'babel.config.js',
